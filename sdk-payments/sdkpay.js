2189add38e18a53246aba02d352aa5b0a6f40c1b
jpcode <jpcardenas@pucp.pe>
2017-09-20 23:32:13 -0500

    'sds'

--- sdk-payments/sdkpay.js
+++ sdk-payments/sdkpay.js
@@ -1,10 +1,49 @@
 (function(){
+
+	var postMessage = function( message, target ){
+
+	}
+
+	var notifyError = function(){
+
+	} 
+
+	var addStyles = function(){
+
+	}
+	
+	var stablishCommunication = function( iframe1, iframe2 ){
+				window.onmessage = function(e){
+				    if (e.data == 'cvv') {
+				    }else if ( e.data == 'cnn'){
+
+				    }else if ( e.data == 'complete' ){
+
+				    }
+				};
+	}
+
+	var postRequest = function(){
+		//connect with node.js 
+	}
+
+	var getDom = function( id ){
+		return document.getElementById( id );
+	}
+
 	var sdkpay = function(){
+		/* factory */
+		var cnn = getDom('cnnid');
+		var cvv = getDom('cvvid');
 		return {
 			init : function(){
-				console.log('init sdk pay');		
+				stablishCommunication( cnn, cvv );
+			},
+			getToken : function(){
+
 			}
 		}
 	};
+	
 	window.Sdkpay = sdkpay();
 })();
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,15 @@
+
+var NodeRSA = require('node-rsa');
+var key = new NodeRSA({b: 512});
+
 var appRouter = function(app) {
 	 app.get("/payments", function(req, res) {
-	 	console.log("payments service requesting");
-	    res.send("done");
+	 	var ccnCvv = '4634030103951029-069';
+		var encrypted = key.encrypt(ccnCvv, 'base64');
+		var decrypted = key.decrypt(encrypted, 'utf8');
+		console.log('decrypted: ', decrypted);
+
+	    res.send(encrypted);
 	});
 }
  
